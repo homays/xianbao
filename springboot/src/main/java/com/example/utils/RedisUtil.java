a694919db0b095533bb15db802d4e97f54f1fc52
Arrebol <2362552845@qq.com>
2024-04-16 21:12:00 +0800

    feat: 引入redis

---
+++ springboot/src/main/java/com/example/utils/RedisUtil.java
@@ -0,0 +1,57 @@
+package com.example.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+public class RedisUtil<T> {
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    /**
+     * 当key值不存在时新增缓存返回true，否则返回false
+     */
+    public boolean setNx(String key, String value) {
+        return redisTemplate.opsForValue().setIfAbsent(key, value);
+    }
+
+    /**
+     * 向redis中set集合添加值，点赞
+     */
+    public void sAdd(String key, String value) {
+        redisTemplate.opsForSet().add(key, value);
+    }
+
+    /**
+     * 向redis中set集合移除，取消点赞
+     */
+    public Long srem(String key, String value) {
+        return redisTemplate.opsForSet().remove(key, value);
+    }
+
+    /**
+     * 判断key，value是否是集中值
+     */
+    public boolean isMember(String key, String value) {
+        return redisTemplate.opsForSet().isMember(key, value);
+    }
+
+    /**
+     * 获取集合大小
+     */
+    public Long size(String key) {
+        return redisTemplate.opsForSet().size(key);
+    }
+
+    /**
+     * 获取集合里所有数据
+     */
+    public Set<String> sMembers(String key) {
+        Set<String> members = redisTemplate.opsForSet().members(key);
+        return members;
+    }
+}
